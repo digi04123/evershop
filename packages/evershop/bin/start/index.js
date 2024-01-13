9465fa14d6e7a8d72351b65c8ffbd72a8f7c9cb7
digi04123 <130883211+digi04123@users.noreply.github.com>
2024-01-13 12:26:46 +0800

    Update index.js to include apm config

--- packages/evershop/bin/start/index.js
+++ packages/evershop/bin/start/index.js
@@ -1,4 +1,11 @@
 // https://github.com/node-config/node-config/issues/578
+
+var apm = require('elastic-apm-node').start({
+  serviceName: 'evershop',
+  serverUrl: 'https://apm.rhel.ontolo.wongfamily.homes',
+  environment: 'prod'
+})
+
 process.env.ALLOW_CONFIG_MUTATIONS = true;
 require('dotenv').config();
 const { start } = require('@evershop/evershop/bin/lib/startUp');
